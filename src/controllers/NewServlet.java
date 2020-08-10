ac3bf13da53759dff7e40b9605e02d75f8561523
suguru.matsuo <t3ntu2jhpyqgibvqwfmc@docomo.ne.jp>
2020-08-10 22:02:16 +0900

    tasklist

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -34,9 +34,9 @@ public class NewServlet extends HttpServlet {
         request.setAttribute("_token", request.getSession().getId());
 
         // おまじないとしてのインスタンスを生成
-        request.setAttribute("message", new Task());
+        request.setAttribute("tasks", new Task());
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
         rd.forward(request, response);
      }
 }
